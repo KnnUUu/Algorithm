d65b2bae148f1ddc82462c6734638be4d78b0ebd
KangmingWu <kangming819@gmail.com>
2019-05-12 20:37:30 +0900

    20190512

--- LC010.py
+++ LC010.py
@@ -1,42 +1,84 @@
 def isMatch(s,p):
-    def dynamic(i,j):
-        if i<len(s) and j>=len(p):
+    if not p: return not s # empty pattern
+    if not s: # empty string
+        if len(p)>=2 and p[1]=='*':
+            return isMatch(s,p[2:])
+        else:
             return False
 
-        elif i>=len(s) and j<len(p):
-            even = j+1
-            while even<len(p):
-                if p[even]=='*':
-                    even += 2
-                else:
-                    return False
-            return even==len(p)-1
+    firstMatch = s[0] == p[0] or p[0]=='.'
+    star = len(p)>=2 and p[1]=='*'
 
-        elif i>=len(s) and j>=len(p):
-            return True
-
+    if firstMatch:
+        if star:
+            return isMatch(s[1:],p) or isMatch(s,p[2:])
         else:
+            return isMatch(s[1:],p[1:])
+    else:
+        if star:
+            return isMatch(s,p[2:])
+        else:
+            return False
+
+def isMatch(s,p):
+    def dp(i,j):
+        if j>=len(p):
+            return i>=len(s)
+        if i>=len(s):
             if j+1<len(p) and p[j+1]=='*':
-                if p[j]=='.':
-                    return True
-                else:
-                    for k in range(i,len(s)):
-                        if s[k]!=p[j]:
-                            break
-                        elif dynamic(k,j+2):
-                            return True
-                    return dynamic(i,j+2)
-
+                return dp(i,j+2)
             else:
-                if s[i]==p[j] or p[j]=='.':
-                    return dynamic(i+1,j+1)
-                else:
-                    return False
+                return False
 
-    return dynamic(0,0)
+        firstMatch = s[i] == p[j] or p[j]=='.'
+        star = j+1<len(p) and p[j+1]=='*'
+
+        if firstMatch:
+            if star:
+                return dp(i+1,j) or dp(i,j+2)
+            else:
+                return dp(i+1,j+1)
+        else:
+            if star:
+                return dp(i,j+2)
+            else:
+                return False
+    return dp(0,0)
+
+def isMatch(s,p):
+    memory = dict()
+    def dp(i,j):
+        if (i,j) in memory: return memory[i,j]
+
+        if j>=len(p):
+            memory[i,j]=i>=len(s)
+            return memory[i,j]
+        if i>=len(s):
+            if j+1<len(p) and p[j+1]=='*':
+                return dp(i,j+2)
+            else:
+                memory[i, j] =False
+                return memory[i,j]
+
+        firstMatch = s[i] == p[j] or p[j]=='.'
+        star = j+1<len(p) and p[j+1]=='*'
+
+        if firstMatch:
+            if star:
+                return dp(i+1,j) or dp(i,j+2)
+            else:
+                return dp(i+1,j+1)
+        else:
+            if star:
+                return dp(i,j+2)
+            else:
+                memory[i, j] = False
+                return memory[i, j]
+    return dp(0,0)
 
 print(isMatch(s = "aa", p = "a")) # false
 print(isMatch(s = "aa", p = "a*")) # true
 print(isMatch(s = "ab", p = ".*")) # true
 print(isMatch(s = "aab", p = "c*a*b")) # true
 print(isMatch(s = "mississippi", p = "mis*is*p*.")) # false
+print(isMatch(s = "ab", p = ".*c")) # false
