49af2f7df6e734b7426f462d5620199b393bc45a
KangmingWu <kangming819@gmail.com>
2019-08-16 19:39:57 +0900

    190816 add 1020,678

--- LC805.py
+++ LC805.py
@@ -1,18 +1,28 @@
 class Solution(object):
-    def splitArraySameAverage(self,A):
-        if len(A) <= 1: return False
+    def splitArraySameAverage(self, A):
+        from fractions import Fraction
+        N = len(A)
+        S = sum(A)
+        A = [z - Fraction(S, N) for z in A]
 
-        def judge(target,sumArr,remain,arr):
-            if remain==0:
-                return sumArr==target
-            if remain>len(arr) or not arr: return False
-            return (judge(target,sumArr+arr[0],remain-1,arr[1:])
-                    or judge(target,sumArr,remain,arr[1:]))
+        if N == 1: return False
 
-        avg = sum(A) / len(A)
-        for lenB in range(1, (len(A) + 1) // 2 + 1):
-            if judge(lenB * avg, 0, lenB, A): return True
-        return False
+        # Want zero subset sum
+        left = {A[0]}
+        for i in range(1, N//2):
+            left = {z + A[i] for z in left} | left | {A[i]}
+        if 0 in left: return True
+
+        right = {A[-1]}
+        for i in range(N//2, N-1):
+            right = {z + A[i] for z in right} | right | {A[i]}
+        if 0 in right: return True
+
+        sleft = sum(A[i] for i in range(N//2))
+        sright = sum(A[i] for i in range(N//2, N))
+
+        return any(-ha in right and (ha, -ha) != (sleft, sright) for ha in left)
+
 
 test = Solution()
 assert test.splitArraySameAverage([0]) == False
